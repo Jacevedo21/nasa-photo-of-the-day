43c6055d04003d134ac8cc43afa44f5d9ae1a15a
Jacevedo21 <jacevedo0213@gmail.com>
2020-07-15 18:45:45 -0400

    still working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
+import Image from './Components/Image'
+
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=u1C8MAdVRpgLgB1ZvwPB4FKk0DmTqDAoZhOmWghX')
+    .then(res => {
+      setData(res.data)
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+  console.log(data)
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Image data={data}/>
     </div>
   );
 }
 
 export default App;
+
+//https://api.nasa.gov/planetary/apod?api_key=u1C8MAdVRpgLgB1ZvwPB4FKk0DmTqDAoZhOmWghX
---
+++ src/Components/Image.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Image(props) {
+    console.log(props.data.url)
+    return (
+        <div>
+                       
+        </div>
+    )
+}
